1e602cd6215cb45252460591f1cf26e452211ec7
Miranda Kastemaa <miranda@foldplop.com>
2019-11-08 15:20:20 +0200

    update triton scripts

--- audio/workdir/triton/generate.sh
+++ audio/workdir/triton/generate.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-N="$1"
-PROJECT_DIR="$2"
+COUNT=$1
+START=$2
+PROJECT_DIR=$3
 
-for ((i=0; i < N; i++)); do
+if [ ! -n "$COUNT" ] || [ ! -n "$START" ] || [ ! -n "$PROJECT_DIR" ]; then
+    echo "usage: generate.sh count start project_dir"
+    exit 1
+fi
+
+for ((i=START; i < START+COUNT; i++)); do
 	nsynth_generate \
 		--checkpoint_path="$PROJECT_DIR/magenta/magenta/models/nsynth/wavenet-ckpt/model.ckpt-200000" \
 		--source_path="$PROJECT_DIR/audio/workdir/embeddings_batched/batch$i" \
 		--save_path="$PROJECT_DIR/audio/workdir/audio_output/batch$i" \
 		--batch_size=2048 \
 		--alsologtostderr \
-		--gpu_number="$i" \
+		--gpu_number="$((i-START))" \
                 --samples_per_save=300000 &
 	pids[${i}]=$!
 done
